07d52461aaddb962bacf082004d5f7df9508fecf
Andrius <>
2019-05-23 10:20:34 +0100

    bar chart showing monthly births added

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,7 +8,8 @@ queue()
         var ndx = crossfilter(salaryData);
        
         show_year(ndx);
-
+        show_month(ndx);
+        
         dc.renderAll();
     }
 /* Bar chart showing yearly births*/
@@ -31,3 +32,46 @@ queue()
             .yAxis().ticks(10);
             
     }
+
+/*Bar chart showing montly births */
+function show_month(ndx){
+
+    var dim = ndx.dimension(dc.pluck('month'));
+    var group = dim.group().reduceSum(dc.pluck('births'));
+
+    dc.rowChart("#month")
+    .width(768)
+    .height(400)
+    .dimension(dim)
+    .group(group)
+    .ordering(function(d) { return +d.key; })
+    .transitionDuration(900)
+    .elasticX(true).group(group)
+    .label(function(d){
+        if(d.key == 1)
+          return "January";
+        if(d.key == 2)
+          return "February";
+        if(d.key == 3)
+          return "March";
+        if(d.key == 4)
+          return "April";
+        if(d.key == 5)
+          return "May";
+        if(d.key == 6)
+          return "June";
+        if(d.key == 7)
+          return "July";
+        if(d.key == 8)
+          return "August";
+        if(d.key == 9)
+          return "September";
+        if(d.key == 10)
+          return "October";
+        if(d.key == 11)
+          return "November";
+        if(d.key == 12)
+          return "December";
+       
+      })
+}
